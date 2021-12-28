b760ff6ddbcd300b4b3c0cf3c46cbc6bad8dd516
= <=>
2021-12-28 16:05:58 +0200

    user tags templates created

---
+++ women/templatetags/women_tags.py
@@ -0,0 +1,32 @@
+from django import template
+from women.models import Women, Category
+
+register = template.Library()
+
+
+@register.simple_tag(name='cats')
+def get_categories(filter_pk=None):
+    if filter:
+        return Category.objects.filter(pk=filter_pk)
+    else:
+        return Category.objects.all()
+
+
+@register.inclusion_tag('women/categories_list.html')
+def show_categories(sort=None, cat_selected=0):
+    if sort:
+        cats = Category.objects.order_by(sort)
+    else:
+        cats = Category.objects.all()
+
+    return {'cat_selected': cat_selected, 'cats': cats}
+
+
+@register.inclusion_tag('women/menu_list.html')
+def get_menu():
+    menu = [{'title': 'О сайте', 'url_name': 'about'},
+            {'title': 'Добавить статью', 'url_name': 'add_page'},
+            {'title': 'Обратная связь', 'url_name': 'contact'},
+            {'title': 'Войти', 'url_name': 'login'}]
+
+    return {'menu': menu}
--- women/views.py
+++ women/views.py
@@ -1,30 +1,21 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
-from .models import *
-
-menu = [{'title': 'О сайте', 'url_name': 'about'},
-        {'title': 'Добавить статью', 'url_name': 'add_page'},
-        {'title': 'Обратная связь', 'url_name': 'contact'},
-        {'title': 'Войти', 'url_name': 'login'}]
+from .models import Women, Category
 
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
 
     context = {'title': 'Главная страница',
-               'menu': menu,
                'posts': posts,
-               'cats': cats,
                'cat_selected': 0}
 
     return render(request, 'women/index.html', context=context)
 
 
 def about(request):
-    context = {'title': 'О сайте',
-               'menu': menu}
+    context = {'title': 'О сайте'}
     return render(request, 'women/about.html', context=context)
 
 
@@ -46,15 +37,12 @@ def read_post(request, post_id):
 
 def read_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404()
 
     context = {'title': 'Рубрики',
-               'menu': menu,
                'posts': posts,
-               'cats': cats,
                'cat_selected': cat_id}
 
     return render(request, 'women/index.html', context=context)
